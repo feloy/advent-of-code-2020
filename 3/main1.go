88847c7fde81935217a75360e33e2653d43ab18a
Philippe Martin <martin.p@sfeir.com>
2020-12-06 06:41:41 +0100

    uncomment first code

--- 3/main1.go
+++ 3/main1.go
@@ -1,24 +1,24 @@
-//package main
-//
-//import (
-//	"bufio"
-//	"fmt"
-//	"os"
-//)
-//
-//func main() {
-//	scanner := bufio.NewScanner(os.Stdin)
-//
-//	found := 0
-//	x := 0
-//	for scanner.Scan() {
-//		line := scanner.Text()
-//		s := line[x]
-//		if s == '#' {
-//			found++
-//		}
-//		x += 3
-//		x = x % len(line)
-//	}
-//	fmt.Printf("%d\n", found)
-//}
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	found := 0
+	x := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		s := line[x]
+		if s == '#' {
+			found++
+		}
+		x += 3
+		x = x % len(line)
+	}
+	fmt.Printf("%d\n", found)
+}
--- 5/main1.go
+++ 5/main1.go
@@ -1,27 +1,26 @@
-//package main
-//
-//import (
-//	"bufio"
-//	"fmt"
-//	"os"
-//	"strconv"
-//	"strings"
-//)
-//
-//func main() {
-//	scanner := bufio.NewScanner(os.Stdin)
-//	max := int64(0)
-//	for scanner.Scan() {
-//		line := scanner.Text()
-//		bin := strings.Replace(line, "B", "1", 10)
-//		bin = strings.Replace(bin, "F", "0", 10)
-//		bin = strings.Replace(bin, "R", "1", 10)
-//		bin = strings.Replace(bin, "L", "0", 10)
-//		i, _ := strconv.ParseInt(bin, 2, 64)
-//		if i > max {
-//			max = i
-//		}
-//	}
-//	fmt.Printf("%d\n", max)
-//}
-//
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	max := int64(0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		bin := strings.Replace(line, "B", "1", 10)
+		bin = strings.Replace(bin, "F", "0", 10)
+		bin = strings.Replace(bin, "R", "1", 10)
+		bin = strings.Replace(bin, "L", "0", 10)
+		i, _ := strconv.ParseInt(bin, 2, 64)
+		if i > max {
+			max = i
+		}
+	}
+	fmt.Printf("%d\n", max)
+}
